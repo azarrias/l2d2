4690289110eb8462da8ccf85a024a7c563fc5455
azarrias <adolfo.zarrias@gmail.com>
2019-12-19 16:06:18 +0100

    Add override to SpriteBatch methods

--- src/libs/l2d2.lua
+++ src/libs/l2d2.lua
@@ -1,5 +1,3 @@
-local l2d2 = { graphics = {} }
-
 -- Workaround to support love.js 0.11.0-rc3 which is a pre-release of v11
 --local love11 = love.getVersion() == 11
 local love11 = love._version_major == 11 or love._version_major == 0 and love._version_minor == 11
@@ -8,9 +6,14 @@ local function round(n)
   return math.floor(n + 0.5)
 end
 
-local _clear
-local _getBackgroundColor, _getColor
-local _setBackgroundColor, _setColor
+local reg = debug.getregistry()
+local SpriteBatch = reg.SpriteBatch
+local _spriteBatchGetColor, _spriteBatchSetColor = SpriteBatch.getColor, SpriteBatch.setColor
+
+local grp = love.graphics
+local _clear = grp.clear
+local _getBackgroundColor, _setBackgroundColor = grp.getBackgroundColor, grp.setBackgroundColor
+local _getColor, _setColor = grp.getColor, grp.setColor
 
 -- Converts color values from 0-1 range to 0-255
 local function normalizedToByte(table)
@@ -39,27 +42,35 @@ local function byteToNormalized(table)
 end
 
 local function overrideGetColor(vanilla, func)
-  vanilla = func
-  return function()
-    local r, g, b, a = vanilla()
-    local t = byteToNormalized({r, g, b, a})
-    return t
+  return function(...)
+    local args = {...}
+    local r, g, b, a = vanilla(args[1])
+    return unpack(byteToNormalized({r, g, b, a}))
   end
 end
 
 local function overrideSetColor(vanilla, func)
-  vanilla = func
   return function(...)
     local args = {...}
-    args = normalizedToByte(args)
-    vanilla(unpack(args))
+    local ud
+    if #args > 0 and type(args[1]) == 'userdata' then
+      ud = args[1]
+      table.remove(args, 1)
+      args = normalizedToByte(args)
+      vanilla(ud, unpack(args))
+    else
+      args = normalizedToByte(args)
+      vanilla(unpack(args))
+    end
   end
 end
 
 if not love11 then
-  love.graphics.clear = overrideSetColor(_clear, love.graphics.clear)
-  love.graphics.getBackgroundColor = overrideGetColor(_getBackgroundColor, love.graphics.getBackgroundColor)
-  love.graphics.getColor = overrideGetColor(_getColor, love.graphics.getColor)
-  love.graphics.setBackgroundColor = overrideSetColor(_setBackgroundColor, love.graphics.setBackgroundColor)
-  love.graphics.setColor = overrideSetColor(_setColor, love.graphics.setColor)
+  grp.clear = overrideSetColor(_clear, grp.clear)
+  grp.getBackgroundColor = overrideGetColor(_getBackgroundColor, grp.getBackgroundColor)
+  grp.setBackgroundColor = overrideSetColor(_setBackgroundColor, grp.setBackgroundColor)
+  grp.getColor = overrideGetColor(_getColor, grp.getColor)
+  grp.setColor = overrideSetColor(_setColor, grp.setColor)
+  SpriteBatch.getColor = overrideGetColor(_spriteBatchGetColor, SpriteBatch.getColor)
+  SpriteBatch.setColor = overrideSetColor(_spriteBatchSetColor, SpriteBatch.setColor)
 end
--- src/main.lua
+++ src/main.lua
@@ -1,22 +1,32 @@
 require 'libs.l2d2'
 
-local MOBILE_OS = (love.system.getOS() == 'Android' or love.system.getOS() == 'OS X')
-local WEB_OS = love.system.getOS() == 'Web'
-local APP_TITLE = 'L2D2 test'
-local FONT_SIZE = 16
+local mobile_os = (love.system.getOS() == 'Android' or love.system.getOS() == 'OS X')
+local web_os = love.system.getOS() == 'Web'
+local app_title = 'L2D2 test'
+local font_size = 16
+local r, g, b, a
 
 function love.load()
   if arg[#arg] == "-debug" then 
     require("mobdebug").start() 
   end
   
-  love.window.setTitle(APP_TITLE)
+  love.window.setTitle(app_title)
   
-  font = love.graphics.newFont(FONT_SIZE)
+  font = love.graphics.newFont(font_size)
   love.graphics.setFont(font)
   os_str = love.system.getOS()
   vmajor, vminor, vrevision, vcodename = love.getVersion()
   v_str = string.format("Love version: %d.%d.%d - %s", vmajor, vminor, vrevision, vcodename)
+  
+  -- this does nothing more than testing the converted color values with instance based functions
+  local width, height = 32, 32
+  local maxsprites = 10
+  imageData = love.image.newImageData(width, height)
+  image = love.graphics.newImage(imageData)
+  spriteBatch = love.graphics.newSpriteBatch(image, maxsprites)
+  spriteBatch:setColor(0.6, 0.6, 0.8)
+  r, g, b, a = spriteBatch:getColor()
 end
 
 function love.draw()
@@ -27,7 +37,7 @@ function love.draw()
   r, g, b, a = love.graphics.getBackgroundColor()
   
   love.graphics.setColor(0.8, 0.8, 0.8)
-  love.graphics.print(APP_TITLE, 20, 20)
+  love.graphics.print(app_title, 20, 20)
   love.graphics.print("LÖVE Library Wrapper for Multiplatform", 20, 40)
   love.graphics.print("  and Version Compatibility", 20, 56)
   love.graphics.setColor(0.8, 0.6, 0.6)
